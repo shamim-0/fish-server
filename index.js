a9520d0c23636bccfae81d09cef4a93505a57a42
shamim-0i <dev.shamim0@gmail.com>
2023-06-14 14:46:24 +0600

    add single venue

--- index.js
+++ index.js
@@ -45,12 +45,12 @@ async function run() {
 
         // single view user 
         app.get('/singleuser', async (req, res) => {
-            const {_id} = req.query;
+            const { _id } = req.query;
             // console.log(id);
-            const query = {_id: new ObjectId(_id)}
+            const query = { _id: new ObjectId(_id) }
             const result = await UserCollection.findOne(query);
             res.send(result);
-          });
+        });
 
         // post user 
         app.post('/user', async (req, res) => {
@@ -61,21 +61,21 @@ async function run() {
         })
 
 
-        app.put('/update-user', async (req, res)=>{
-        const _id = req.query._id;
-        const data = req.body;
-        const query = { _id: new ObjectId(_id) };
-        const options = { upsert: true };
-        const updateDoc = {
-            $set: {
-                Batch_tag: data.batch,
-                status: data.status,
-                role: data.role,
-            }
-        };
-        const result = await UserCollection.updateOne(query, updateDoc, options);
-        res.send(result)
-        console.log(data)
+        app.put('/update-user', async (req, res) => {
+            const _id = req.query._id;
+            const data = req.body;
+            const query = { _id: new ObjectId(_id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    Batch_tag: data.batch,
+                    status: data.status,
+                    role: data.role,
+                }
+            };
+            const result = await UserCollection.updateOne(query, updateDoc, options);
+            res.send(result)
+            console.log(data)
         });
 
 
@@ -83,33 +83,33 @@ async function run() {
 
         // get user by email 
 
-        app.get('/user-email', async (req, res)=>{
+        app.get('/user-email', async (req, res) => {
             const email = req.query.email;
-            const query = {email : email}
+            const query = { email: email }
             const result = await UserCollection.findOne(query);
             res.send(result);
         })
-        
+
 
 
         // fish chart collection 
-        app.post('/fist_chart', async (req, res)=>{
+        app.post('/fist_chart', async (req, res) => {
             const data = req.body;
             const result = await fishChartCollection.insertOne(data);
             res.send(result)
         });
 
-        app.get('/fist_chart', async (req, res)=>{
+        app.get('/fist_chart', async (req, res) => {
             const query = {};
             const data = fishChartCollection.find(query);
-            const result =await data.toArray();
+            const result = await data.toArray();
             res.send(result);
         })
 
 
-        app.delete('/fish_chart_delete', async (req, res)=>{
+        app.delete('/fish_chart_delete', async (req, res) => {
             const id = req.query.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await fishChartCollection.deleteOne(query);
             res.send(result)
             // console.log(id)
@@ -122,27 +122,27 @@ async function run() {
         // venue 
 
         // add category for venue
-        app.post('/venue-category-add', async (req, res)=>{
-            const data =  req.body;
+        app.post('/venue-category-add', async (req, res) => {
+            const data = req.body;
             const result = await venueCategoryCollection.insertOne(data);
             res.send(result);
         })
-        
+
         // get venue category 
 
-        app.get('/venue-category-get', async (req, res)=>{
+        app.get('/venue-category-get', async (req, res) => {
             const query = {};
             const data = venueCategoryCollection.find(query);
-            const result =await data.toArray();
+            const result = await data.toArray();
             res.send(result);
         })
-  
+
 
         // delete venue category 
 
-        app.delete('/venue-category-delete' , async (req, res)=> {
+        app.delete('/venue-category-delete', async (req, res) => {
             const _id = req.query._id;
-            const query = {_id: new ObjectId(_id)};
+            const query = { _id: new ObjectId(_id) };
             const result = await venueCategoryCollection.deleteOne(query);
             res.send(result);
         })
@@ -157,27 +157,27 @@ async function run() {
         //  ---------------------------------------------------// add sub category for venue ----------------------------------------------------
 
 
-         app.post('/venue-sub-category-add', async (req, res)=>{
-            const data =  req.body;
+        app.post('/venue-sub-category-add', async (req, res) => {
+            const data = req.body;
             const result = await venueSubCategoryCollection.insertOne(data);
             res.send(result);
         })
-        
+
         // get venue category 
 
-        app.get('/venue-sub-category-get', async (req, res)=>{
+        app.get('/venue-sub-category-get', async (req, res) => {
             const query = {};
             const data = venueSubCategoryCollection.find(query);
-            const result =await data.toArray();
+            const result = await data.toArray();
             res.send(result);
         })
-  
+
 
         // delete venue category 
 
-        app.delete('/venue-sub-category-delete' , async (req, res)=> {
+        app.delete('/venue-sub-category-delete', async (req, res) => {
             const _id = req.query._id;
-            const query = {_id: new ObjectId(_id)};
+            const query = { _id: new ObjectId(_id) };
             const result = await venueSubCategoryCollection.deleteOne(query);
             res.send(result);
         })
@@ -186,18 +186,75 @@ async function run() {
 
         // ------------------------------------ venue collection ------------------------------------------------------------------------
 
-        app.post('/venue', async (req, res)=>{
+        app.post('/venue', async (req, res) => {
             const data = req.body;
-            const result  =  await venueCollection.insertOne(data);
+            const result = await venueCollection.insertOne(data);
             res.send(result);
         });
 
-        app.get('/venue', async (req, res)=>{
-            const query = {};
-            const data = venueCollection.find(query);
-            const result = await data.toArray();
-            res.send(result);
+        app.get('/venue', async (req, res) => {
+            const filter = req.query;
+
+            if (filter.all) {
+                console.log(filter)
+                const query = {};
+                const data = venueCollection.find(query);
+                const result = await data.toArray();
+                res.send(result);
+            }
+            else if (filter.districtSelect) {
+                const districtSelect = filter.districtSelect;
+                const query = { districtSelect: districtSelect };
+                const data = venueCollection.find(query);
+                const result = await data.toArray();
+                res.send(result);
+            }
+            else {
+                res.send([{ error: 'No Access data ' }])
+            }
+
         })
+
+
+        // get single venue 
+
+        app.get('/single-venue', async (req, res) => {
+            const _id = req.query._id;
+            const query = { _id: new ObjectId(_id) };
+            const data = await venueCollection.findOne(query);
+            res.send(data);
+        })
+
+
+
+
+        //-------------------------------------------------------------------------- requested for catch and release fish data
+        app.put('/test-venue', async (req, res) => {
+            const { _id } = req.query;
+            const data = {status: 'Applied For Tasting'}
+            const query = { _id: new ObjectId(_id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: data.status,
+                }
+            };
+            const result = await venueCollection.updateOne(query, updateDoc, options);
+            res.send(result)
+            console.log(data)
+        })
+
+
+        // testing venue list 
+
+        // Applied For Tasting
+
+        app.get('/test-venue', async (req, res) => {
+            const query = { status: "Applied For Tasting" };
+            const data = await venueCollection.find(query).toArray();
+            res.send(data);
+          });
+          
 
 
 
